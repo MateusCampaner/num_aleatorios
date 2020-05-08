22b5a227c14ee2b289a36cc4bb7cfcef3562c920
Mateus Campaner <mateuscampanercontato@gmail.com>
2020-05-08 16:39:16 -0300

    teste de funcionamento

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-window.alert("esta funcionando?")
-
 function numAleatorio(min = 0, max = 1000) {
     let valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+
     let num1 = document.getElementById("num1")
     let num2 = document.getElementById("num2")
-    
+
+    let res = { min: num1, max: num2}
+
     
 }
+
