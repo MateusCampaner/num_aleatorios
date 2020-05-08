cdcb760061f670695451512ac826035fab05923b
Mateus Campaner <mateuscampanercontato@gmail.com>
2020-05-08 16:23:06 -0300

    script adicionado

---
+++ script.js
@@ -0,0 +1,6 @@
+function numAleatorio(min = 0, max = 1000) {
+    let valor = Math.random() * (max - min) + min
+    let num1 = document.getElementById("num1")
+    let num2 = document.getElementById("num2")
+    
+}
